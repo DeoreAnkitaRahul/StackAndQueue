695a7ad60be6fcbd913c6e97f16d77774f461a8d
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-27 19:35:13 +0530

    Create Stack using Linked List

--- Program.cs
+++ Program.cs
@@ -5,7 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue Program");
-            
+            Console.WriteLine("Create Stack using Linked List");
+
+            StackUsingLinkedList stack = new StackUsingLinkedList();
+            stack.Push(50);
+            stack.Push(20);
+            stack.Push(80);
+            stack.Display();
         }
     }
 }
